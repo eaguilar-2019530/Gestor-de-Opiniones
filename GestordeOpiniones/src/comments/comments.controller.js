5623ed493bf46251789091186ffaff0db10c257a
eaguilar-2019530 <eaguilar-2019530@kinal.edu.gt>
2024-03-02 10:16:54 -0600

    Parte Final

--- GestordeOpiniones/src/comments/comments.controller.js
+++ GestordeOpiniones/src/comments/comments.controller.js
@@ -10,9 +10,35 @@ export const test = (req, res)=>{
 
 export const save = async(req,res)=>{
     try {
-        
+        let data = req.body
+        let publi = await Public.findOne({_id: data.Tilte})
+        if(!publi) return res.status(404).send({message: 'Keeper no funciona'})
+        let comment = new Comment(data)
+        await comment.save()
+        return res.send({message: 'Commentario guardado correctamente'})
     } catch (err) {
         console.error(err)
         return res.send(500).send({message: 'Error al guardar comentario'})
     }
 }
+
+export const update = async(req, res)=>{
+    try{
+        let { id } = req.params
+        let data = req.body
+        let update = checkUpdate(data, false)
+        if(!update) return res.status(400).send({message: 'Have submitted some data that cannot be updated or missing data'})
+        let updatedAnimal = await Animal.findOneAndUpdate(
+            {_id: id},
+            data,
+            {new: true}
+        ).populate('keeper', ['name', 'phone'])
+        if(!updatedAnimal) return res.status(404).send({message: 'Animal not found, not updated'})
+        return res.send({message: 'Animal updated successfully', updatedAnimal})
+    }catch(err){
+        console.error(err)
+        return res.status(500).send({message: 'Error updating animal'})
+    }
+}
+
+
